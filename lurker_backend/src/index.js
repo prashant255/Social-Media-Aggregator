a1da3d8371203d611637955371b25938e7e3c932
prashant <prashant.agrawal@litmusblox.io>
2021-01-28 12:08:39 +0530

    Basic backend setup(Node js) #4

---
+++ lurker_backend/src/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const morgan = require('morgan') //For logging API calls
+const bodyParser = require('body-parser')
+
+const app = express()
+
+//Handling CORS so that frontend can use API's on some other port
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Aceess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// const LP1router = require('./routers/LP-1')
+// const LP2router = require('./routers/LP-2')
+
+//port is provided in the environment variable.
+const port = process.env.PORT
+
+app.use(express.json())
+// app.use('/api/Oanda', LP1router)
+// app.use('/api/ICMarket', LP2router)
+// app.use('/api/common', AllLProuter)
+
+app.get('/test', (req, res) => {
+    res.send("Test successful")
+})
+
+app.get('*', (req, res) => {
+    res.send('404 page not found!').status(404)    
+})
+
+app.listen(port, () => {
+    console.log('Server is up and running on port: ' + port)
+})
