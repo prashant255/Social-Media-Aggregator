a7e23b41a24b800cbb6229de892321b203ed9ae3
Swapnil Markhedkar <swapnilmarkhedkar@gmail.com>
2021-03-11 22:35:53 +0530

    first login backend #19

--- lurker_backend/src/routers/reddit.js
+++ lurker_backend/src/routers/reddit.js
@@ -2,19 +2,29 @@ const express = require('express')
 const router = new express.Router;
 const redditAuth = require('../service/redditAuth')
 const authenticateUser = require('../authentication/authMiddleware')
-const redditPosts = require('../service/redditPosts')
+const redditPosts = require('../service/redditPosts');
 
 router.get('/connect', (req, res) => {
     redditAuth.authorizeUser(req, res);
 });
 
 router.post('/callback', authenticateUser, async (req, res) => {
-    try{
-        await redditAuth.saveToken(req, res);
-        res.send()
-    } catch({message}) {
+
+    const { code } = req.body;
+
+    redditAuth.redditCallback(code).then(response => {
+        const redditObj = {
+            redditAccessToken: response.refresh_token,
+            redditRefreshToken: response.access_token
+        };
+        
+        redditAuth.saveToken();
+        res.send();
+
+    }).catch(message => {
+        // TODO: check?
         errorHandler(message, res)
-    }
+    });
 })
 
 router.post('/allPosts', authenticateUser, async (req, res) => {
