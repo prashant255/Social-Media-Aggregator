614def53279ce9e38e1c384e2244db927115f56b
Swapnil Markhedkar <swapnilmarkhedkar@gmail.com>
2021-03-12 15:22:23 +0530

    linkSocial Media Reddit initial changes(breaks)#19

--- lurker_backend/src/service/redditAuth.js
+++ lurker_backend/src/service/redditAuth.js
@@ -3,6 +3,7 @@ const request = new XMLHttpRequest();
 const common = require('../common')
 const Token = require('../models/tokens')
 const error = require('../error')
+const axios = require('axios')
 
 authorizeUser = (req, res) => {
 
@@ -12,6 +13,7 @@ authorizeUser = (req, res) => {
             response_type:"code",
             state:process.env.REDDIT_STATE,
             redirect_uri:"http://localhost:3000/callback/reddit",
+            // redirect_uri:"http://localhost:3000/callback/reddit",
             duration:"permanent",
             scope:"read"
         }
@@ -25,36 +27,60 @@ getBase64Auth = () => {
 }
 
 redditCallback = (code) => {
-    
-    return new Promise((resolve, reject) => {
-        const url = "https://www.reddit.com/api/v1/access_token?grant_type=authorization_code&code=" + code + "&redirect_uri=http://localhost:8080/api/reddit/callback";
-        request.open("POST", url, true);
+    console.log("redditCallback");
+
+    return new Promise( async (resolve, reject) => {
+        const redirectUri = "http://localhost:3000/callback/reddit";
+        const url = "https://www.reddit.com/api/v1/access_token?grant_type=authorization_code&code=" + code + "&redirect_uri=" + redirectUri;
         const base64Auth = getBase64Auth();    
-        request.setRequestHeader('Authorization','Basic ' + base64Auth);
-        request.setRequestHeader("Content-type", "application/json");
-
-        request.send();
-
-        request.onreadystatechange = () => {
-            if (request.readyState == request.DONE) {
-                const response = JSON.parse(request.responseText);
-
-                if(request.status == 200){
-                    resolve(response);
-                }else{
-                    reject(response);
-                }
-            }
-        }
-
-        request.onerror = (e) => {
-            // TODO: Mostly no error till this point, but you never know
-            reject({
-                message: e,
-                error: request.status
-            });
-        }
+        const headers = {
+            "Authorization": 'Basic ' + base64Auth,
+            "Content-type": 'application/json'
+        };
+        const body = {}
+        console.log('time to start');
+        await axios.post(url, body ,{headers}).then(response => {
+            // console.log(response);
+            resolve(response.data);
+        }).catch(e => {
+            console.log(e);
+            reject(e);
+        });
     });
+    
+    // return new Promise((resolve, reject) => {
+    //     const redirectUri = "http://localhost:8080/api/reddit/callback";
+    //     const url = "https://www.reddit.com/api/v1/access_token?grant_type=authorization_code&code=" + code + "&redirect_uri=" + redirectUri;
+    //     const base64Auth = getBase64Auth();   
+        
+    //     request.open("POST", url, true);
+    //     request.setRequestHeader('Authorization','Basic ' + base64Auth);
+    //     request.setRequestHeader("Content-type", "application/json");
+
+    //     request.send();
+
+    //     request.onreadystatechange = () => {
+    //         if (request.readyState == request.DONE) {
+    //             const response = JSON.parse(request.responseText);
+
+    //             if(request.status == 200){
+    //                 resolve(response);
+    //             }else{
+    //                 console.log("ERROR: ", response);
+    //                 reject(response);
+    //             }
+    //         }
+    //     }
+
+    //     request.onerror = (e) => {
+    //         // TODO: Mostly no error till this point, but you never know
+    //         console.log("ERR: ",e);
+    //         reject({
+    //             message: e,
+    //             error: request.status
+    //         });
+    //     }
+    // });
 }
 
 getRefreshedAccessToken = (refreshToken) => {
