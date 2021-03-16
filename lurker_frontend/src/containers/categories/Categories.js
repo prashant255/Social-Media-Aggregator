3e4137677ed04f007a97ce38d60475e9460f58fc
ShrutiCodes <shrutidphadke@gmail.com>
2021-03-17 00:18:51 +0530

    categories refined

--- lurker_frontend/src/containers/categories/Categories.js
+++ lurker_frontend/src/containers/categories/Categories.js
@@ -1,48 +1,82 @@
 import React, {Component} from 'react'
-import classes from './Categories.module.css'
-import Cards from '../../components/ui/cardsCategory/CardsCategory'
-import Button from '../../components/ui/button/Button'
+
+import classes from '../categories/Categories.module.css'
+
+import Card from '../../components/ui/cardsCategory/CardsCategory'
+
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import GridList from '@material-ui/core/GridList';
 
 class Categories extends Component {
-
-
     state = {
         categories:{
-            sports: {
-                displayName: "Sports",
-                iconName: "football",
+            entertainment: {
+                displayName: "Entertainment",
+                imageSource: "entertainment.png",
                 selected: false    
             },
-            business: {
-                displayName: "Business",
-                iconName: "business",
+            celebrity: {
+                displayName: "Celebrity",
+                imageSource: "celebrities.png",
                 selected: false    
             },
-            celeberity: {
-                displayName: "Celeberity",
-                iconName: "star",
+            politics: {
+                displayName: "Politics",
+                imageSource: "politics.png",
                 selected: false    
             },
             finance: {
                 displayName: "Finance",
-                iconName: "wallet",
+                imageSource: "finance.png",
                 selected: false    
             },
             gaming: {
                 displayName: "Gaming",
-                iconName: "game-controller",
+                imageSource: "gaming.png",
                 selected: false    
             },
-            entertainment: {
-                displayName: "Entertainment",
-                iconName: "tv",
+            travel: {
+                displayName: "Travel",
+                imageSource: "travel.png",
+                selected: false    
+            },
+            health: {
+                displayName: "Health & Wellness",
+                imageSource: "health.png",
+                selected: false    
+            },
+            motivation: {
+                displayName: "Motivation",
+                imageSource: "motivation.png",
+                selected: false    
+            },
+            promo: {
+                displayName: "Promotions",
+                imageSource: "promos.png",
+                selected: false    
+            },
+            sports: {
+                displayName: "Sports",
+                imageSource: "sports.png",
+                selected: false    
+            },
+            tech: {
+                displayName: "Technology",
+                imageSource: "tech.png",
+                selected: false    
+            },
+            business: {
+                displayName: "Business",
+                imageSource: "business.png",
                 selected: false    
             }
         },
         selectedCount: 0
     }
 
-    categoryClickedHanlder = (id) => {
+    categoryClickedHandler = (id) => {
+        console.log(id)
         let categories = {...this.state.categories}
         let selectedCount = this.state.selectedCount
         let categoryConfig = {...categories[id]}
@@ -55,7 +89,7 @@ class Categories extends Component {
         })
     }
 
-    nextClickHanlder = () => {
+    nextClickHandler = () => {
         const categorySelected = []
         for(let categoryIdentifier in this.state.categories)
             if(this.state.categories[categoryIdentifier].selected)
@@ -72,28 +106,50 @@ class Categories extends Component {
                 config: this.state.categories[key]
             })
         }
+
         return (
-            <div className = {classes.Categories}>
-                <div className = {classes.Cards}>
-                    {
-                        categoriesArray.map(category => 
-                            <Cards
-                                id = {category.id}    
-                                categoryName = {category.config.displayName}
-                                iconName = {category.config.iconName}
-                                selected = {category.config.selected}
-                                clicked = {() => this.categoryClickedHanlder(category.id)}
-                            />
-                        )
-                    }
-                </div>
-                <div className = {classes.Center}>
-                    <Button 
-                        btnType = "Success" 
-                        disabled = {!this.state.selectedCount} 
-                        clicked = {this.nextClickHanlder}>NEXT</Button>
-                </div>
+            <div>
+                <GridList cellHeight={280} cols={4}>
+                    <GridListTile cols={4} style={{ height: 'auto', color:'rgba(17, 53, 117, 1)'}}>
+                        <h1 style={{textAlign: 'center', fontSize: '60px'}}>What would you like to see?</h1>
+                    </GridListTile>
+
+                    {categoriesArray.map((category) => (
+                    <GridListTile cols={1} key={category.id}>
+                        <Card 
+                            id={category.id}
+                            categoryName = {category.config.displayName}
+                            imageSource = {category.config.imageSource}
+                            selected = {category.config.selected}
+                            clicked = {() => this.categoryClickedHandler(category.id)}
+                        />
+                        <GridListTileBar title={category.config.displayName} style={{backgroundColor:'rgba(17, 53, 117, 1)'}} />
+                    </GridListTile>
+                    ))}
+                </GridList>
             </div>
+
+            // <div className = {classes.Categories}>
+            //     <div className = {classes.Cards}>
+            //         {
+            //             categoriesArray.map(category => 
+            //                 <Cards
+            //                     id = {category.id}    
+            //                     categoryName = {category.config.displayName}
+            //                     iconName = {category.config.iconName}
+            //                     selected = {category.config.selected}
+            //                     clicked = {() => this.categoryClickedHanlder(category.id)}
+            //                 />
+            //             )
+            //         }
+            //     </div>
+            //     <div className = {classes.Center}>
+            //         <Button 
+            //             btnType = "Success" 
+            //             disabled = {!this.state.selectedCount} 
+            //             clicked = {this.nextClickHanlder}>NEXT</Button>
+            //     </div>
+            // </div>
         )
     }
 }
