3825f493582a2b1bfb818063fbffdddb6cee8105
Prashant <prashantsa255@gmail.com>
2021-05-20 00:24:14 +0530

    Settings Page(Frontend) #26

--- frontend/src/components/ui/header/Header.js
+++ frontend/src/components/ui/header/Header.js
@@ -19,6 +19,7 @@ import SettingsIcon from '@material-ui/icons/Settings';
 import InfoIcon from '@material-ui/icons/Info';
 
 import Logout from '../../logout/Logout'
+import DeleteProfileModal from '../modal/deleteProfile/DeleteProfile'
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -44,6 +45,7 @@ const Header = (props) => {
 
 	const [anchorEl, setAnchorEl] = React.useState(null);
 	const [showLogoutComponent, setShowLogoutComponent] = React.useState(false);
+	const [deleteProfileOpen, setDeleteProfileOpen] = React.useState(false);
 
 	const userName = useSelector(state => state.name)
 
@@ -87,13 +89,22 @@ const Header = (props) => {
 		},
 	}))(MenuItem);
 
+	const handleOpenModal = (state) => {
+		setDeleteProfileOpen(state);
+	  };
 	const logoutClickHandler = () => {
 		setShowLogoutComponent(true)
+	}
+
+	const deleteAccountHanlder = () => {
+		handleClose()
+		setDeleteProfileOpen(true)
 	}
 
 	return (
 		<div className={classes.root}>		
 		{showLogoutComponent? <Logout /> : null}
+		<DeleteProfileModal open = {deleteProfileOpen} handleModal = { handleOpenModal}/>
 			<AppBar position="fixed">
 				<Toolbar className={classes.headerColor}>
 					<div className={classes.title}>
@@ -120,6 +131,15 @@ const Header = (props) => {
 								</ListItemIcon>
 								<ListItemText primary="Settings" />
 							</StyledMenuItem>
+
+							{/* TODO: To make submenu inside the settings for delete accout */}
+							<StyledMenuItem onClick = { () => deleteAccountHanlder()}>
+								<ListItemIcon>
+									<SettingsIcon fontSize="small" />
+								</ListItemIcon>
+								<ListItemText primary="Delete Account" />
+							</StyledMenuItem>
+
 							<StyledMenuItem>
 								<ListItemIcon>
 									<InfoIcon fontSize="small" />
