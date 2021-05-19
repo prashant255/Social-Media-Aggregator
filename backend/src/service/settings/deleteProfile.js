216734e7acfd88098ba2bfbe4803e604437d7aaa
Prashant <prashantsa255@gmail.com>
2021-05-19 20:12:26 +0530

    Settings Page Delete Account #26

---
+++ backend/src/service/settings/deleteProfile.js
@@ -0,0 +1,30 @@
+const Posts = require('../../models/posts')
+const Tokens = require('../../models/tokens')
+const Users = require('../../models/users')
+
+const deleteProfile = async (userId) => {
+    const t = await sequelize.transaction();
+    try{
+        await Posts.destroy({
+            where: { userId }
+        }, { transaction: t })
+
+        await Tokens.destroy({
+            where: { userId }, 
+        }, {transaction: t})
+
+        await Users.destroy({
+            where: { id: userId }   
+        }, {transaction: t})
+
+        await t.commit()
+
+    } catch(e) {
+        await t.rollback()
+        throw new Error(e.message)
+    }
+}
+
+module.exports = {
+    deleteProfile
+}
