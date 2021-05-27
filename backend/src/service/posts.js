6ac96a49237c8f5a74e246c542f0feb8a6a40278
Prashant <prashantsa255@gmail.com>
2021-05-27 20:00:10 +0530

    Update Query to group by duplicates #60

--- backend/src/service/posts.js
+++ backend/src/service/posts.js
@@ -1,4 +1,5 @@
 const categories = require('../service/settings/categories')
+const Post = require('../models/posts')
 const { QueryTypes } = require('sequelize')
 
 const getPostForUser = async (userId,offset) => {
@@ -6,7 +7,12 @@ const getPostForUser = async (userId,offset) => {
         const selectedCategories = (await categories.getUserCategories(userId)).selectedCategory
         const limit = 5
         // let query = 'select * from posts as p inner join post_details as pd on p."lurkerPostId" = pd.id where p."userId" = :userId and LOWER(pd.category) in (:category)'
-        let query = 'select * from posts p inner join groups g on p."groupId" = g.id inner join post_details pd on p."lurkerPostId"=pd.id where p."userId" = :userId and LOWER(g.category) in (:category) '
+        let query = `select array_agg(ARRAY[cast(p."lurkerPostId" as text), pd."postId", pd."handle", cast(p.bookmark as text)]) 
+        from posts p 
+        inner join groups g on p."groupId" = g.id 
+        inner join post_details pd on p."lurkerPostId"=pd.id 
+        where p."userId" = :userId and LOWER(g.category) in (:category)
+        group by g.id order by g.id `
         query += `limit ${limit} offset ${offset}`
 
         const posts = await sequelize.query(query, 
@@ -17,7 +23,20 @@ const getPostForUser = async (userId,offset) => {
             },
             type: QueryTypes.SELECT 
         })
-        return posts    
+        let res = []
+        posts.map( ele => {
+            let groupedPost = []
+            ele["array_agg"].map( eleOfGroup => {
+                groupedPost.push({
+                    "lurkerPostId": eleOfGroup[0],
+                    "posdId": eleOfGroup[1],
+                    "handle": eleOfGroup[2],
+                    "bookmark": eleOfGroup[3]
+                })
+            })
+            res.push(groupedPost)
+        })
+        return res    
     }
     catch(e) {
         throw new Error(e.message)
